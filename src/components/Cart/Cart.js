efd8dd9b12c2fdc53a58de8931467ce5ec004543
Marcus Francis <marcusmagnusson@outlook.com>
2019-05-20 02:15:16 +0200

    Display total

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,10 +1,36 @@
-import React from "react";
+import React, { useContext } from "react";
 import RenderCart from 'components/Cart/RenderCart'
+import { Store } from 'store';
 
 export default function Cart() {
+    const { state, dispatch } = useContext(Store);
+    const Price = state.map(product => {
+        return product.price;
+        });
+
+        const Total = Price.reduce((previous, product) => {
+        return (product += previous);
+        }, 0);
   return (
     <>
       <RenderCart />
+
+ 
+          <div class="ui cards">
+            <div class="card">
+              <div class="middle aligned content">
+                <div class="header">Subtotal is {"$" + Total}</div>{" "}
+                <button
+                  class="small ui right floated button"
+                  onClick={() => alert("Give me a call :)")}
+                >
+                  Checkout
+                  <i class="right chevron icon" />
+                </button>{" "}
+                <div class="meta"> Procceed </div>
+              </div>{" "}
+            </div>{" "}
+          </div>
     </>
   );
 }
