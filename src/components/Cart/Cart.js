8a393d7d1a4da72cf2039da654554334b4b2d8db
Marcus Francis <marcusmagnusson@outlook.com>
2019-05-21 03:46:50 +0200

    "Cart is emtpy"

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,25 +1,38 @@
 import React, { useContext } from "react";
-import RenderCart from 'components/Cart/RenderCart'
-import { Store } from 'store';
+import RenderCart from "components/Cart/RenderCart";
+import { Store } from "store";
+import { Link } from "react-router-dom";
 
 export default function Cart() {
-    const { state } = useContext(Store);
-    const Price = state.map(product => {
-        return product.price;
-        });
+  const { state } = useContext(Store);
+  const Price = state.map(product => {
+    return product.price;
+  });
 
-        const Total = Price.reduce((previous, product) => {
-        return (product += previous);
-        }, 0);
+  const Total = Price.reduce((previous, product) => {
+    return (product += previous);
+  }, 0);
   return (
     <>
       <RenderCart />
 
- 
-          <div class="ui cards">
-            <div class="card">
-              <div class="middle aligned content">
-                <div class="header">Subtotal is {"$" + Total}</div>{" "}
+      <div class="ui cards">
+        <div class="card">
+          <div class="middle aligned content">
+            {!Total ? (
+              <>
+                <div class="header">Cart is empty</div>
+                <Link to="/">
+                  <button class="small ui right floated button">
+                    Back to store
+                    <i class="right chevron icon" />
+                  </button>{" "}
+                </Link>
+                <div class="meta"> </div>
+              </>
+            ) : (
+              <>
+                <div class="header">Subtotal is {"$" + Total}</div>
                 <button
                   class="small ui right floated button"
                   onClick={() => alert("Give me a call :)")}
@@ -28,9 +41,11 @@ export default function Cart() {
                   <i class="right chevron icon" />
                 </button>{" "}
                 <div class="meta"> Procceed </div>
-              </div>{" "}
-            </div>{" "}
-          </div>
+              </>
+            )}
+          </div>{" "}
+        </div>{" "}
+      </div>
     </>
   );
 }
